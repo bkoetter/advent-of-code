9f190621df46e3880f5df960f31e6923ca760228
Björn Kötter <git@bk.netburn.de>
2024-01-21 17:49:42 +0100

    half-way there

--- aoc-2023-03-2/src/main.rs
+++ aoc-2023-03-2/src/main.rs
@@ -4,18 +4,17 @@ fn main() -> std::io::Result<()> {
     file.lines().enumerate().map(
         |(_, line)| line.chars().enumerate().filter_map(|(i, c)| match c {
             '*' => {
-                match re.find_iter(line).find(|m| {
+                re.find_iter(line).find(|m| {
                     let start = if m.start() > 0 { m.start() - 1 } else { m.start() };
                     let end = if m.end() < line.len() { m.end() + 1 } else { m.end() };
                     (start..end).contains(&i)
-                }) {
-                    Some(m) => Some(m.as_str().parse::<usize>().unwrap()),
-                    _ => None,
-                }
+                }).map(|m| m.as_str().parse::<usize>().unwrap())
             }
             _ => None,
         }).collect::<Vec<usize>>()
-    ).for_each(|line| {
+    )
+    .filter(|v| v.len() > 0)
+        .for_each(|line| {
         println!("{:?}", line);
     });
     Ok(())
