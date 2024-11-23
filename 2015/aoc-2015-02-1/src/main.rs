b58f4e62868c73b7d1c896afe0f073c21534d863
Björn Kötter <git@bk.netburn.de>
2024-11-23 12:58:16 +0100

    initial commit

--- 2015/aoc-2015-02-1/src/main.rs
+++ 2015/aoc-2015-02-1/src/main.rs
@@ -1,3 +1,25 @@
+use std::io::BufRead;
+
+fn calc_size(input: &[u8]) -> u64 {
+    _ = input
+        .lines()
+        .map(|s| s.unwrap().splitn(3,'x')
+            .map(|n| n.parse::<u8>().unwrap())
+            .collect::<Vec<u8>>()
+        ).for_each(|s| println!("{:?}", s));
+    5
+}
+
 fn main() {
+    calc_size(include_bytes!("../input.txt"));
     println!("Hello, world!");
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn try_size() {
+        assert_eq!(58, calc_size(b"6x12x8"));
+    }
 }
