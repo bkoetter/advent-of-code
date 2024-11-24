6f9c1c69d983679a4deef54836e9b42e4d4a9ef8
Björn Kötter <git@bk.netburn.de>
2024-11-24 20:19:53 +0100

    final version with correct result

--- 2015/aoc-2015-02-1/src/main.rs
+++ 2015/aoc-2015-02-1/src/main.rs
@@ -1,14 +1,41 @@
 use std::io::BufRead;
 
-fn calc_size(input: &[u8]) -> u16 {
+#[derive(Debug)]
+struct Square {
+    l: u32,
+    w: u32,
+    h: u32,
+}
+
+fn calc_size(input: &[u8]) -> u32 {
     input
         .lines()
-        .map(|s| s.unwrap().splitn(3,'x')
-            .map(|n| n.parse::<u16>().unwrap())
-            .collect::<Vec<u16>>()
-        )
-        .map(|array| array.iter().min().unwrap() + array.iter().sum::<u16>())
-        .sum()
+        .filter_map(|line| line.ok())
+        .map(|line| {
+            line.splitn(3, 'x')
+                .fold(Square { l: 0, w: 0, h: 0 }, |acc, field| match acc {
+                    Square { l: 0, w: 0, h: 0 } => Square {
+                        l: field.parse().unwrap(),
+                        w: acc.w,
+                        h: acc.h,
+                    },
+                    Square { l: _, w: 0, h: 0 } => Square {
+                        l: acc.l,
+                        w: field.parse().unwrap(),
+                        h: acc.h,
+                    },
+                    Square { l: _, w: _, h: 0 } => Square {
+                        l: acc.l,
+                        w: acc.w,
+                        h: field.parse().unwrap(),
+                    },
+                    _ => panic!("This must never happen"),
+                })
+        })
+        .fold(0, |acc, square| {
+            acc + (square.l * square.w + square.w * square.h + square.h * square.l) * 2
+            + [square.l * square.w, square.w * square.h, square.h * square.l].iter().min().unwrap()
+        })
 }
 
 fn main() {
@@ -19,7 +46,11 @@ fn main() {
 mod tests {
     use super::*;
     #[test]
-    fn try_size() {
-        assert_eq!(58, calc_size(b"6x12x8"));
+    fn try_size_1() {
+        assert_eq!(58, calc_size(b"2x3x4"));
+    }
+    #[test]
+    fn try_size_2() {
+        assert_eq!(43, calc_size(b"1x1x10"));
     }
 }
