447e2c0707244b6b6a21d5247ffb507ef57540fa
Björn Kötter <git@bk.netburn.de>
2024-12-20 19:34:33 +0100

    Latest state and working 2025-04

---
+++ 2015/aoc-2015-04-2/src/main.rs
@@ -0,0 +1,26 @@
+fn get_md5(input: &str) -> Option<(String, u64)> {
+    (1..=u64::MAX)
+        .map(|i| {
+            let digest = md5::compute(format!("{}{}", input, i));
+            (format!("{:x}", digest), i)
+        })
+        .find(|s| s.0.starts_with("000000"))
+}
+
+fn main() {
+    match get_md5("bgvyzdsv") {
+        None => println!("No matching number found."),
+        Some((.., magic_number)) => println!("{magic_number}"),
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_get_md5() {
+        assert_eq!(get_md5("abcdef").unwrap().1, 609043);
+        assert_eq!(get_md5("pqrstuv").unwrap().1, 1048970);
+    }
+}
