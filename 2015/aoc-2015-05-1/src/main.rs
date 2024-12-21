4ccaa79f121dd1b14277a9e1dac99513a9deadb0
Björn Kötter <git@bk.netburn.de>
2024-12-21 10:14:21 +0100

    2015 day 5 part 1 working, but ugly coding

--- 2015/aoc-2015-05-1/src/main.rs
+++ 2015/aoc-2015-05-1/src/main.rs
@@ -1,3 +1,58 @@
+use std::io::BufRead;
+
+fn get_nice_stings(input: &[u8]) -> u16 {
+    let mut nice_string_count = 0;
+    for line in input.lines().map(|line| line.unwrap()) {
+        if line.chars().filter(|c| ['a', 'e', 'i', 'o', 'u'].contains(c)).count() < 3 {
+            continue
+        }
+        let mut previous_char: char = ' ';
+        let mut is_char_same = false;
+        for char in line.chars() {
+            if char.eq(&previous_char) {
+                is_char_same = true;
+                break;
+            }
+            previous_char = char;
+        }
+        if !is_char_same {
+            continue;
+        }
+        if line.contains("ab") || line.contains("cd") || line.contains("xy") || line.contains("pq")
+        {
+            continue;
+        }
+        nice_string_count += 1;
+    }
+    nice_string_count
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("{}", get_nice_stings(include_bytes!("../input.txt")));
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_get_nice_stings_1() {
+        assert_eq!(get_nice_stings(b"ugknbfddgicrmopn"), 1)
+    }
+    #[test]
+    fn test_get_nice_stings_2() {
+        assert_eq!(get_nice_stings(b"aaa"), 1)
+    }
+    #[test]
+    fn test_get_nice_stings_3() {
+        assert_eq!(get_nice_stings(b"jchzalrnumimnmhp"), 0)
+    }
+    #[test]
+    fn test_get_nice_stings_4() {
+        assert_eq!(get_nice_stings(b"haegwjzuvuyypxyu"), 0)
+    }
+    #[test]
+    fn test_get_nice_stings_5() {
+        assert_eq!(get_nice_stings(b"dvszwmarrgswjxmb"), 0)
+    }
 }
