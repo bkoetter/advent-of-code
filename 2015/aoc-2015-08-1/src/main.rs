1c32afb4c42b0fb4943a2d9528d8749b39daeaac
Björn Kötter <git@bk.netburn.de>
2025-03-02 07:32:07 +0100

    working solution using regex

--- 2015/aoc-2015-08-1/src/main.rs
+++ 2015/aoc-2015-08-1/src/main.rs
@@ -1,11 +1,27 @@
-fn count_chars(input: &[u8]) -> usize {
-    let string = String::from_utf8(input.to_vec()).unwrap();
-        println!("{string}");
-    2
+use evalexpr::*;
+use regex::Regex;
+use std::process::exit;
+
+fn count_chars(input: &str) -> usize {
+    let re = Regex::new(r#"\\x[0-9a-f]"#).unwrap();
+    input
+        .lines()
+        .map(|line| (line.len(), re.replace_all(line, "")))
+        .map(|line| match eval_string(&line.1) {
+            Err(e) => {
+                eprintln!("{e}");
+                exit(1)
+            }
+            Ok(string) => {
+                // println!("'{string}' -> {} characters", string.len());
+                line.0 - string.len()
+            }
+        })
+        .sum()
 }
 
 fn main() {
-    count_chars(r#""""#.as_ref());
+    println!("{}", count_chars(include_str!("../input.txt")));
 }
 
 #[cfg(test)]
@@ -14,14 +30,23 @@ mod tests {
 
     #[test]
     fn test_1() {
-        assert_eq!(count_chars(r#""""#.as_ref()), 2)
+        assert_eq!(count_chars(r#""""#), 0)
     }
     #[test]
     fn test_2() {
-        assert_eq!(count_chars(b"abc"), 3)
+        assert_eq!(count_chars(r#""abc""#), 3)
     }
     #[test]
     fn test_3() {
-        assert_eq!(count_chars(b"aaa\"aaa"), 7)
+        assert_eq!(count_chars(r#""aaa\"aaa""#), 7)
+    }
+    #[test]
+    fn test_4() {
+        // assert_eq!(count_chars(r#""\x27""#), 1)
+        assert_eq!("\x27".len(), 1)
+    }
+    #[test]
+    fn test_5() {
+        assert_eq!(count_chars(r#""\"""#), 1)
     }
 }
