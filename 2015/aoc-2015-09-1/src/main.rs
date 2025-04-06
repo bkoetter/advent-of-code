b2fd5c2b8043def49c87afa2f6f8612d0d0ff767
Björn Kötter <git@bk.netburn.de>
2025-04-06 06:55:24 +0200

    getting closer

--- 2015/aoc-2015-09-1/src/main.rs
+++ 2015/aoc-2015-09-1/src/main.rs
@@ -1,33 +1,64 @@
+use itertools::{Itertools, sorted};
+use std::collections::HashMap;
 use std::str::FromStr;
-use itertools::Itertools;
 
 fn main() {
     let input = include_str!("../input.txt");
-    let cities = get_cities(input);
-    // let list = ["Dublin", "London", "Belfast"].into_iter().permutations(3).collect::<Vec<_>>();
-    println!("{:?}", cities.iter().permutations(5).collect::<Vec<_>>());
+    let distances_between_cities = get_distances_between_cities(input);
+    let cities = get_cities(distances_between_cities.clone());
+    for (i, city) in distances_between_cities.keys().enumerate() {
+        println!("{}: {} {}", i + 1, city.0, city.1);
+    }
+    for (i, city) in cities.iter().enumerate() {
+        println!("{}: {city}", i + 1)
+    }
+    for list in cities.iter().permutations(cities.len()).collect::<Vec<_>>() {
+        println!("{:?}", list)
+    }
+    println!(
+        "{}",
+        cities
+            .iter()
+            .permutations(cities.len())
+            .collect::<Vec<_>>()
+            .len()
+    )
 }
 
-// struct City String;
 type City = String;
-type 
+type Distance = u32;
+type CityDistances = HashMap<(City, City), Distance>;
 
-fn get_cities(distances: Vec<Distance>) -> Vec<City> {
-    // input.lines().map(|l| l.parse().unwrap()).collect::<Vec<Distance>>()
-    distances.iter().unique().collect::<Vec<_>>()
+fn get_cities(city_distances: CityDistances) -> Vec<City> {
+    city_distances
+        .into_keys()
+        .flat_map(|cities| [cities.0, cities.1])
+        .unique()
+        .collect()
 }
 
-#[derive(Debug)]
-struct Distance
+fn get_distances_between_cities(input: &str) -> CityDistances {
+    input
+        .lines()
+        .map(|line| line.parse::<CityDistance>().unwrap())
+        .map(|cd| (cd.cities, cd.distance))
+        .collect::<CityDistances>()
+}
 
-impl FromStr for Distance {
+struct CityDistance {
+    cities: (City, City),
+    distance: Distance,
+}
+
+impl FromStr for CityDistance {
     type Err = std::io::Error;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         let parts = s.split_whitespace().collect::<Vec<_>>();
-        Ok(Distance {
-            city1: parts[0].to_string(),
-            city2: parts[2].to_string(),
+        Ok(CityDistance {
+            cities: sorted([parts[0].to_string(), parts[2].to_string()])
+                .collect_tuple()
+                .unwrap(),
             distance: parts[4].parse::<u32>().unwrap(),
         })
     }
